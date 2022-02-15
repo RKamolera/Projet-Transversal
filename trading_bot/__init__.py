7f6d44767fa035ac11169ccf6834db5fddc76eca
Muhammet951 <98944447+Muhammet951@users.noreply.github.com>
2022-02-15 13:55:36 +0100

    Revert "Revert "commit""
    This reverts commit 1b3994380394d685f02669161c6cebc7937eee40.

--- trading_bot/__init__.py
+++ trading_bot/__init__.py
@@ -1,1 +1,7 @@
+import plotly.graph_objects as go
 
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+fig = go.Figure([go.Scatter(x=df['Date'], y=df['AAPL.High'])])
+fig.show()
